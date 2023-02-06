dd294e1d2f135352a00baa12f66b00a8ff682b47
Caroline Miller <caroline.brendle.miller@gmail.com>
2023-02-06 15:20:57 -0600

    adding notes on python syntax

---
+++ python_syntax.py
@@ -0,0 +1,31 @@
+## Python Syntax
+
+# python is case sensitive
+
+# some words have specific uses and must be used with that in mind
+
+# paired symbols - also know as delimiters - surround protions of code
+
+# special characters may need to be escaped through a backslash
+
+## Variables
+
+# store data
+# data is associated with a name
+# can be accessed and used and also can be modified
+# "=" setting a value of a variable 
+
+my_number = 12
+my_cohort = "O'Neil"
+
+# can also update variables
+# >>> a = 12
+# >>> a
+# 12
+
+# a = a + 1
+# now the variable a = 13
+
+## Jupyter Notebook
+
+# interactive computing platform
